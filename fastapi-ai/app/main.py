e6db952a5e77caf607713c6f097daab0ff9faebc
hyomin <leehm123456@gmail.com>
2025-09-18 14:42:43 +0900

    fix: import 에러 수정

--- fastapi-ai/app/main.py
+++ fastapi-ai/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import target, arrow, webrtc
 from app.frame_manager import start_receiver
-from app.core.config import config
+from app.core import config
 
 
 
